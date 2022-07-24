b6de5563e0906471a2e1ee097466b0b67e3e5135
fbellelli <85805299+fbellelli@users.noreply.github.com>
2022-07-24 14:34:37 +0200

    speed improvement and bug fix in finc_countrycol

--- R/find_countrycol.R
+++ R/find_countrycol.R
@@ -5,8 +5,8 @@
 #' For the purpose of this function, a country is any of the 249 territories designated in the ISO standard \code{3166}.
 #' @param x A data frame object
 #' @param return_index A logical value indicating whether the function should return the index of country columns instead of the column names. Default is \code{FALSE}, column names are returned.
-#' @param allow_NA Logical value indicating whether to country columns to contain \code{NA} values. Default is \code{allow_NA=FALSE}, the function will not return country column containing \code{NA} values.
-#' @param min_share A value between \code{0} and \code{1} indicating the minimum share of country names in columns that are returned. A value of \code{0} will return any column containing a country name. A value of \code{1} will return only column whose entries are all country names. Default is \code{0.9}, i.e. at least 90 percent of the column entries need to be country names.
+#' @param allow_NA Logical value indicating whether columns containing \code{NA} values are to be considered as country columns. Default is \code{allow_NA=FALSE}, the function will not return country column containing \code{NA} values.
+#' @param min_share A value between \code{0} and \code{1} indicating the minimum share of country names in columns that are returned. A value of \code{0} will return any column containing a country name. A value of \code{1} will return only column whose entries are all country names. Default is \code{0.9}, i.e. at least 90 percent of the column entries need to be country names (on large dataset this value is estimated from a sample).
 #' @return Returns a vector of country names (\code{return_index=FALSE}) or column indices (\code{return_index=TRUE}) of columns containing country names.
 #' @seealso \link[countries]{is_country} \link[countries]{country_name} \link[countries]{find_timecol}
 #' @export
@@ -21,33 +21,39 @@ find_countrycol <- function(x, return_index=FALSE, allow_NA=FALSE, min_share=0.9
   if(!is.numeric(min_share)|is.null(min_share)|length(min_share)>1|is.na(min_share)|min_share>1|min_share<0){stop("Argument - min_share - needs to be a numeric value between 0 and 1 indicating the requested minimum share of country names in the output columns")}
 
   #take sample if table is large
-  if (nrow(x)>2000){
-    if (nrow(na.omit(x))>2000){x <- na.omit(x)}   #ensures that at least 500 rows are kept
-    x <- x[sample(1:nrow(x), size= min(2000 + sqrt(nrow(x)), nrow(x)) ,replace = FALSE), ]
+  if (nrow(x)>100000){
+    #temp <- na.omit(x)
+    #if (nrow(temp)>100000){x <- temp}   #ensures that at least 500 rows are kept
+    x <- x[sample(1:nrow(x), size= 100000, replace = FALSE), ]
   }
 
-  #exclude NA columns if requested
+  #identify columns containing NA values and eliminate them if requested
   cols <- colnames(x)
   if(allow_NA == FALSE){
-    cols <- cols[!apply(is.na(x[,cols]), MARGIN=2,FUN=any, simplify=TRUE)]
+    cols <- cols[!apply(is.na(x), MARGIN=2,FUN=any, simplify=TRUE)]
+    x <- x[,cols]
   }
-  x <- x[,cols]
-
-  #transform any factor column to character
-  x[sapply(x, is.factor)] <- lapply(x[sapply(x, is.factor)], as.character)
 
   #initiate empty vector of country indices
   country_cols <- NULL
 
-  #loop over all columns
-  for (i in 1:ncol(x)){
-    #check if it is a character vector
-    if(is.character(x[,i])){
-      #if there are too many country names, assume it cannot be a country column
-      #if (length(unique(x[,i]))<=300/min_share){
+  #proceed only if there is at least a column to check
+  if (length(cols)>0){
+
+
+    #transform any factor column to character
+    x[sapply(x, is.factor)] <- lapply(x[sapply(x, is.factor)], as.character)
+
+    #loop over all columns
+    for (i in 1:ncol(x)){
+      #check if it is a character vector
+      if(is.character(x[,i])){
+        #if there are too many country names, assume it cannot be a country column
+        #if (length(unique(x[,i]))<=300/min_share){
         #test if it contains country names
         if(sum(is_country(x[,i]), na.rm=TRUE) >= min_share * length(x[!is.na(x[,i]),i])) country_cols <- c(country_cols, i)
-      #}
+        #}
+      }
     }
   }
 
