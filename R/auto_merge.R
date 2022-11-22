a35f224f60e5c5ec62b3aab66e8c47bbfd885c94
fbellelli <85805299+fbellelli@users.noreply.github.com>
2022-11-22 23:56:56 +0100

    progress on auto_merge function

--- R/auto_merge.R
+++ R/auto_merge.R
@@ -5,10 +5,10 @@
 #' @param by A list or a vector indicating the columns to be used for merging the data. If not provided, the function will automatically detect country and time columns and attempt to merge them. Other columns will be merged only if they share the same name. First time column and first two distinct country columns. Vector of regex, or list of column names, name of element in vector or list can be used to decide name in destination table. e.g. c("countries"="(Nation)|(COUNTRY)", "sector"="[Ii]ndustry") or alternatively list("countries"=c("Nation",NA,"COUNTRY","Nation",NA), "sector"=c("Industry","industry",NA,NA,NA)). In case of list the arguments must be of the same length of data tables to merge and names order must correspond to order of datasets. In case of Regex, it matches with first one.
 #' @param country_to Nomenclature to which country names should be converted to in the output. Default is \code{simple}. For a description of possible options, refer to the table in the vignette \href{https://fbellelli.github.io/countries/articles/dealing_with_names.html}{Dealing with country names}.
 #' @param inner_join Logical value indicating whether to perform an inner join. The default is \code{FALSE}, which results in a full join of the provided tables.
-#' @param verbose Logical value indicating if status messages should be printed to the console. Default is \code{TRUE}.
 #' @param merging_info Logical value. If \code{TRUE}, the function will output a list containing the merged data and information generated during the merging process, such as the conversion table used for country names or information on table variables.
 #' @import tidyr dplyr fastmatch utils stringr
-auto_join <- function(... , by=NULL, country_to="ISO3", coalesce=FALSE, inner_join = FALSE , verbose=TRUE, merging_info = TRUE){
+#' @importFrom lubridate parse_date_time
+auto_merge <- function(... , by=NULL, country_to="ISO3", inner_join = FALSE , merging_info = TRUE){
 
   ############################################################
   #CAPTURE INPUT DATA ----------------------------------------
@@ -23,6 +23,7 @@ auto_join <- function(... , by=NULL, country_to="ISO3", coalesce=FALSE, inner_jo
   ############################################################
   # CHECK INPUTS ---------------------------------------------
 
+  if (length(data)<2) stop("At least two tables need to be provided for merging")
   if (any(sapply(data, ncol)==0) | any(sapply(data, nrow)==0)) stop("Unable to proceed: input data tables need to have at least one column and one row")
   if (!is.atomic(by)&!is.list(by)) stop("Function argument - by - is invalid. It needs to be either a vector of regular expressions, or a list of column names. Refer to the documentation for more information.")
   if (is.list(by)){
@@ -31,10 +32,9 @@ auto_join <- function(... , by=NULL, country_to="ISO3", coalesce=FALSE, inner_jo
     if (sapply(by,length)) stop("Function argument - by - is invalid. Length of name vectors differ from the number of provided tables.")
   }
   if (!is.character(country_to)|length(country_to)!=1|!(country_to %in% colnames(country_reference_list)))stop("The argument - country_to - is invalid. It needs to be one of the nomenclatures names recognised by the function country_name (e.g. ISO3, UN_en, simple, etc...). Refer to the documentation for more information.")
-  if (!is.logical(coalesce) | length(coealesce)!=1) stop("Function argument - coalesce - needs to be a logical statement (TRUE/FALSE)")
   if (!is.logical(inner_join) | length(inner_join)!=1) stop("Function argument - inner_join - needs to be a logical statement (TRUE/FALSE)")
-  if (!is.logical(verbose) | length(verbose)!=1) stop("Function argument - verbose - needs to be a logical statement (TRUE/FALSE)")
   if (!is.logical(merging_info) | length(merging_info)!=1) stop("Function argument - merging_info - needs to be a logical statement (TRUE/FALSE)")
+
 
   ############################################################
   # CHECK WIDE FORMAT AND PIVOT IF NECESSARY ------------------
@@ -54,13 +54,13 @@ auto_join <- function(... , by=NULL, country_to="ISO3", coalesce=FALSE, inner_jo
   ############################################################
   # CLEAN BY ORDER -------------------------------------------
 
-  temp<-parse.by.order(data=data, by=by, col_names=col_names)
+  temp <- parse.by.order(data=data, by=by, col_names=col_names)
   by <- temp$by
   by_types <- temp$by_types
 
 
   ############################################################
-  #PREPARE FOR MERGING ---------------------------------------
+  #PREPARE DATA FOR MERGING ----------------------------------
 
   #CONVERT COLUMN NAMES TO DESTINATION NAME ----
 
@@ -120,11 +120,47 @@ auto_join <- function(... , by=NULL, country_to="ISO3", coalesce=FALSE, inner_jo
 
   #CONVERT COUNTRY NAMES ----
 
-
+  #country columns
+  for (i in which(!is.na(by_table$country))){
+    data[[i]]$country <- country_name(data[[i]]$country, to = "final", custom_table = country_conversion)
+  }
+  #country2 columns
+  if (any("country2" %in% colnames(by_table))){
+    for (i in which(!is.na(by_table$country2))){
+      data[[i]]$country2 <- country_name(data[[i]]$country2, to = "final", custom_table = country_conversion)
+    }
+  }
 
 
   #UNIFY FORMAT OF TIME COLUMNS ----
 
+  #proceed only if there are time columns
+  time_conversion <- NULL
+  if (any("time" %in% colnames(by_table))){
+
+    #check if all time columns are years
+    temp <- TRUE
+    for (i in which(!is.na(by_table$time))){
+      if (countries:::is.yearcol(data[[i]]$time) == FALSE){
+        temp <- FALSE
+        break
+      }
+    }
+
+    #if all time columns are years then proceed with merging, otherwise all time columns need to be converted to a standardised format
+    if (temp == FALSE){
+      time_unformatted <- NULL
+      date_formats <- c("dmy","mdy","ymd","y","my","m","dm","md")
+      for (i in which(!is.na(by_table$time))){
+        time_unformatted <- unique(c(time_unformatted, as.character(data[[i]]$time)))
+        data[[i]]$time <- lubridate::parse_date_time(data[[i]]$time, date_formats)
+      }
+
+      #save info on time formatting
+      time_conversion <- data.frame(time_unformatted = sort(time_unformatted),
+                                    time_formatted = lubridate::parse_date_time(sort(time_unformatted), date_formats))
+    }
+  }
 
 
 
@@ -134,25 +170,33 @@ auto_join <- function(... , by=NULL, country_to="ISO3", coalesce=FALSE, inner_jo
   #PERFORM MERGING -------------------------------------------
 
   #allow for inner / full merging
+  final <- data[[1]]
+  for (i in 2:length(data)){
+    cat(paste0("Performing merging: ",i-1,"/",length(data)-1,"\n"))
+    if (inner_join == TRUE){
+      final <- suppressMessages(inner_join(final, data[[i]]))
+    } else {
+      final <- suppressMessages(full_join(final, data[[i]]))
+    }
+  }
 
+  #TODO:
+  # somehow, save the "by" used in the dplyr merging
+  # improve output function (opzione merging_info / simplify)
+  # improve message on status merging (maybe make progress bar) / calculations -> introduce a verbose setting?
 
 
 
   ############################################################
   #PREPARE OUTPUT --------------------------------------------
 
-  #dare opzione una tabella sola? "COALESCE"? O forse
-  #in tal caso dare opzione scelta livello aggregation?
-
   #se merging_info TRUE, append:
-  #conversion table used for country names
-  #key_cols (generato se - by - non fornito)
-  #by iniziale/finale usato per il merging
-  #by_types
-  #country_conversion
-  #info su paesi/time?
+  #by iniziale usato per il merging
   #warnings?
 
 
-  return(list(by, by_types))
+  return(list(merged_table = final,
+              info_merged_columns = by_table,
+              info_country_names = country_conversion[,-2],
+              info_time_formats = time_conversion))
 }
